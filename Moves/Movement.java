a3d061de08e7f45344cca14e253da937a1f60647
Phil Aldridge <105776682+PhilAldridge@users.noreply.github.com>
2023-10-13 13:13:53 +0100

    Delete Moves directory

--- Moves/Movement.java
+++
@@ -1,7 +0,0 @@
-package src.Moves;
-
-import src.PositionVector;
-
-public interface Movement {
-    public PositionVector go(String direction);
-}
--- Moves/Rotation.java
+++
@@ -1,5 +0,0 @@
-package src.Moves;
-
-public interface Rotation {
-    public String go(String direction);
-}
